e0de776e94adebc8650b37e7e7f5e0c8a913f998
ZinRicky <richynizzac@gmail.com>
2024-12-10 23:59:11 +0100

    Primo giro di file

---
+++ download_videos_from_influencers.py
@@ -0,0 +1,19 @@
+from apify_client import ApifyClient
+import os
+
+client = ApifyClient("apify_api_tfZRPfve7F2Fh67QP1cpkBzgXXqlaD1kkyMb")
+
+profiles = ['tradwifetori', 'ivyoutwest']
+
+for influencer in profiles:
+    if f'{influencer}.json' not in os.listdir('./profiles'):
+        run_input = {
+            'profiles': [influencer],
+            'oldestPostDate': '2024-01-01',
+            'resultsPerPage': 700,
+        }
+
+        run = client.actor("clockworks/tiktok-profile-scraper").call(run_input=run_input)
+
+        with open(os.path.join('profiles', f'{influencer}.json'), 'wb') as out_file:
+            out_file.write(client.dataset(run["defaultDatasetId"]).get_items_as_bytes(item_format='json'))
