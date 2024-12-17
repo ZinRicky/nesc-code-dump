b18e6dc400671983a3b0ea78d5838c098d632bef
ZinRicky <richynizzac@gmail.com>
2024-12-17 19:25:01 +0100

    Commenti per tutti!

--- download_comments_from_videos_apify.py
+++ download_comments_from_videos_apify.py
@@ -10,13 +10,14 @@ if not os.path.isdir('./comments'):
 i = 1 # TO BE CHANGED IF NEEDED
 df: pd.DataFrame = pd.read_csv(os.path.join('polished_data', f'scrape{i}.csv'))
 
-client = ApifyClient("apify_api_eGtf6QOosLv0J87H2PX9sZaEXjpz6f4tyDqJ")
+client = ApifyClient("apify_api_D3gp6G3sI5bzaxWDaQgI0fvCbHCvAg48QacH")
 
 for video, url in tqdm.tqdm(zip(df['id'], df['url'])):
     if not os.path.isfile(os.path.join('comments', f'{video}.json')):
         run_input = {
             'postURLs': [url],
             'commentsPerPost': 5000,
+            'maxRepliesPerComment': 1000
         }
         run = client.actor(
             "clockworks/tiktok-comments-scraper").call(run_input=run_input)
