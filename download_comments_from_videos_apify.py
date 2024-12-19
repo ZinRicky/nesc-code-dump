78d8faa75b35e0ec41adc1674f17c2f3ea8a52bf
ZinRicky <richynizzac@gmail.com>
2024-12-19 21:18:45 +0100

    Scaricati 1/14 dei commenti.

--- download_comments_from_videos_apify.py
+++ download_comments_from_videos_apify.py
@@ -7,20 +7,20 @@ from apify_client import ApifyClient
 if not os.path.isdir('./comments'):
     os.mkdir('./comments')
 
-i = 1 # TO BE CHANGED IF NEEDED
+i = 1  # TO BE CHANGED IF NEEDED
 df: pd.DataFrame = pd.read_csv(os.path.join('polished_data', f'scrape{i}.csv'))
 
-client = ApifyClient("apify_api_D3gp6G3sI5bzaxWDaQgI0fvCbHCvAg48QacH")
+client = ApifyClient("apify_api_DoNS8cjIkxpfViBkaFlXYrGkGBxXsE4trDKx")
 
-for video, url in tqdm.tqdm(zip(df['id'], df['url'])):
-    if not os.path.isfile(os.path.join('comments', f'{video}.json')):
+for video in tqdm.tqdm(df.itertuples(), total=df.shape[0]):
+    if not os.path.isfile(os.path.join('comments', f'{video.id}.json')):
         run_input = {
-            'postURLs': [url],
+            'postURLs': [video.url],
             'commentsPerPost': 5000,
             'maxRepliesPerComment': 1000
         }
         run = client.actor(
             "clockworks/tiktok-comments-scraper").call(run_input=run_input)
-        with open(os.path.join('comments', f'{video}.json'), 'wb') as out_file:
+        with open(os.path.join('comments', f'{video.id}.json'), 'wb') as out_file:
             out_file.write(client.dataset(
                 run["defaultDatasetId"]).get_items_as_bytes(item_format='json'))
