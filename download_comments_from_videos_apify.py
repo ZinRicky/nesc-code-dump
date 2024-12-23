c11a685a66f192145789f9a810c0e2c4318f2ed9
ZinRicky <richynizzac@gmail.com>
2024-12-23 18:10:38 +0100

    Manca davvero poco!

--- download_comments_from_videos_apify.py
+++ download_comments_from_videos_apify.py
@@ -9,7 +9,7 @@ if not os.path.isdir('./comments'):
 
 i = 4  # TO BE CHANGED IF NEEDED
 df: pd.DataFrame = pd.read_csv(os.path.join('polished_data', f'scrape{i}.csv'))
-client = ApifyClient("apify_api_Wit3luOYrpC8aAVxUbcL6IpvkR5wLY0CUpLz")
+client = ApifyClient("apify_api_yaUC6EH8yUUVayrmdSH2bIazHEIdww3FELxC")
 
 for video in tqdm(df.sort_values('comments').itertuples(), total=df.shape[0]):
     if not os.path.isfile(os.path.join('comments', f'{video.id}.json')):
