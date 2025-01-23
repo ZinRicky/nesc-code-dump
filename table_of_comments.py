4aedce2d64202044a5c325268d86d6e6a5a49edf
ZinRicky <richynizzac@gmail.com>
2025-01-23 22:43:46 +0100

    Altri commenti

--- table_of_comments.py
+++ table_of_comments.py
@@ -5,15 +5,16 @@ import csv
 from collections import defaultdict
 from tqdm import tqdm
 
-df: None | pd.DataFrame = None
-videos = pd.read_csv(os.path.join('polished_data', 'videos_from_influencers.csv'))
+videos = pd.read_csv(os.path.join(
+    'polished_data', 'videos_from_influencers.csv'))
 relevant_videos = videos.loc[videos['nation'] == 'US'].id
+data: defaultdict[str, list] = defaultdict(list)
 
 for video in tqdm(relevant_videos, total=relevant_videos.shape[0]):
     if f'{video}.json' in os.listdir('comments'):
         with open(os.path.join('comments', f'{video}.json'), encoding='utf-8') as fp:
             raw_data = json.load(fp)
-        data: defaultdict[str, list] = defaultdict(list)
+
         for comment in raw_data:
             if 'cid' in comment and 'uniqueId' in comment and 'createTime' in comment:
                 data['id'].append(comment['cid'])
@@ -21,15 +22,12 @@ for video in tqdm(relevant_videos, total=relevant_videos.shape[0]):
                 data['author'].append(comment['uniqueId'])
                 data['date'].append(comment['createTime'])
                 data['likes'].append(comment['diggCount'])
-                data['replies'].append(int(comment['replyCommentTotal']) if comment['replyCommentTotal'] else 0)
+                data['replies'].append(
+                    int(comment['replyCommentTotal']) if comment['replyCommentTotal'] else 0)
                 data['reply_of'].append(comment['repliesToId'])
                 data['text'].append(comment['text'])
-                
-        if df is None:
-            df = pd.DataFrame(data)
-        else:
-            df = pd.concat([df, pd.DataFrame(data)])
 
-if df is not None:
-    df.to_csv(os.path.join('polished_data', 'comments_from_videos.csv'),
-              index=False, quoting=csv.QUOTE_NONNUMERIC)
+df = pd.DataFrame(data)
+
+df.to_csv(os.path.join('polished_data', 'comments_from_videos.csv'),
+          index=False, quoting=csv.QUOTE_NONNUMERIC)
